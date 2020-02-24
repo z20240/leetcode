0e49768667b910ed7d1e95de46ea05c4808c730e
les <les.lee@slotfactory.com>
2020-02-24 18:43:11 +0800

    two-sum

---
+++ Two Sum/index.js
@@ -0,0 +1,36 @@
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    const l = nums.length;
+    let next_i = -1;
+    for (let i = 0 ; i < l ; i++) {
+        const num = nums[i];
+
+        next_i = nums.slice(i+1, l).findIndex(n => num + n === target);
+
+        if (next_i >= 0) return [i, i+1+next_i];
+    }
+
+    return []
+
+};
+
+
+console.log(twoSum([3, 2, 3], 6))
